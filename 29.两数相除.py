cbeecbc7dcdae1e652b5f2264a362e48f0ac28ef
xiaomingaaa <13693963350@163.com>
2021-04-16 19:22:26 +0800

    new commit

--- 29.两数相除.py
+++ 29.两数相除.py
@@ -12,13 +12,35 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
+        ### 在这一题中，无法使用python的sys.maxsize(),这个与计算机位数有关，对于64位的系统，最大表示为2^64-1
+        MAX_INT_32=2147483647
+        MIN_INT_32=-2147483648
         import sys
-        if dividend==0:
-            return 0
-        if divisor==1:
-            return dividend
-        if divisor==-1 and dividend==(-sys.maxsize-1):
-            return 
         
+        if divisor==-1 and dividend==MIN_INT_32:
+            return MAX_INT_32
+        sign=1
+        if (dividend>0 and divisor<0) or (dividend<0 and divisor>0):
+            sign=-1
+        if dividend>0:
+            dividend=-dividend
+        if divisor>0:
+            divisor=-divisor
+        if dividend>divisor:
+            return 0
+        result=self.div(dividend,divisor)
+        if sign==-1:
+            result=-result
+        return result
+        
+    def div(self,a,b):
+        if a>b:
+            return 0
+        count=1
+        tb=b
+        while tb+tb>a and tb+tb<0:
+            tb=tb+tb
+            count=count+count
+        return count+self.div(a-tb,b)
 # @lc code=end
 
