ea239a40c9cada1bd0fb0ccf78094e1d13991f69
xiaomingaaa <13693963350@163.com>
2021-04-18 20:03:52 +0800

    new commit

--- 75.颜色分类.py
+++ 75.颜色分类.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2021-04-18 18:56:40
+LastEditTime: 2021-04-18 20:03:40
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /leetcode/75.颜色分类.py
+'''
 #
 # @lc app=leetcode.cn id=75 lang=python3
 #
@@ -10,5 +18,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        pass
 # @lc code=end
 
