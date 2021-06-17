5f975a09f4c82088028846848e8bf2e58f26d709
xiaomingaaa <13693963350@163.com>
2021-06-17 17:44:12 +0800

    new commit

---
+++ 32.最长有效括号.py
@@ -0,0 +1,19 @@
+'''
+Author: your name
+Date: 2021-06-05 19:35:53
+LastEditTime: 2021-06-10 13:39:59
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \leetcode\32.最长有效括号.py
+'''
+#
+# @lc app=leetcode.cn id=32 lang=python3
+#
+# [32] 最长有效括号
+#
+
+# @lc code=start
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        
+# @lc code=end
